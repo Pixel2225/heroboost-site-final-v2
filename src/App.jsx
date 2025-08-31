a2fb7a8442994c240cb1c65bc6c21362749e0854
pixel2225 <francescopocholle2225@gmail.com>
2025-08-31 12:35:27 +0200

    fix: clean App.jsx to remove missing components

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,6 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
-import Home from './components/Home'; // Assurez-vous que ce composant existe
-import Games from './components/Games'; // Assurez-vous que ce composant existe
-import Contact from './components/Contact'; // Assurez-vous que ce composant existe
-import Dashboard from './components/Dashboard'; // Assurez-vous que ce composant existe
+import Dashboard from './components/Dashboard';
 import Login from './components/Login';
 import Register from './components/Register';
 import BrawlStars from './components/BrawlStars';
@@ -14,19 +11,15 @@ function App() {
   return (
     <Router>
       <div>
-        {/* Vous pouvez mettre votre barre de navigation ici si vous en avez une */}
         <nav>
-          <Link to="/">Accueil</Link>
-          <Link to="/jeux">Jeux</Link>
-          <Link to="/contact">Contact</Link>
           <Link to="/tableau-de-bord">Tableau de bord</Link>
           <Link to="/login">Login</Link>
+          <Link to="/register">Register</Link>
+          <Link to="/brawl-stars">Brawl Stars</Link>
+          <Link to="/clash-royale">Clash Royale</Link>
         </nav>
 
         <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/jeux" element={<Games />} />
-          <Route path="/contact" element={<Contact />} />
           <Route path="/tableau-de-bord" element={<Dashboard />} />
           <Route path="/login" element={<Login />} />
           <Route path="/register" element={<Register />} />
